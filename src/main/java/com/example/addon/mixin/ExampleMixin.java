f7d4b8a8ffe32048c22469cfa39270e688feeaca
gezersertac481-cmyk <gezersertac481@gmail.com>
2026-02-05 20:25:46 +0300

    Update ExampleMixin.java

--- src/main/java/com/example/addon/mixin/ExampleMixin.java
+++ src/main/java/com/example/addon/mixin/ExampleMixin.java
@@ -1,9 +1,5 @@
 package com.example.addon.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.client.MinecraftClient;
-
-@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-    // Bu dosya artık boş ve hata vermez.
+    // Boş bırakıldı, artık hata veremez.
 }
